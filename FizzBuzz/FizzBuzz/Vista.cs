6b9718fac6b19d3a3912898dcd35a8d156b62692
Kevin <pkevin.walsh@gmail.com>
2017-07-14 01:16:25 -0600

    Se crearon los metodos para mostrar el juego, mostrar que se perdio y mostrar el numero

--- FizzBuzz/FizzBuzz/Vista.cs
+++ FizzBuzz/FizzBuzz/Vista.cs
@@ -17,8 +17,34 @@ namespace FizzBuzz
 
         public void menuInicial()
         {
+            Console.Clear();
             Console.WriteLine("Bienvenido al juego FIZZ BUZZ \n");
             Console.WriteLine("Presione Enter para iniciar");
         }
+
+        public void respuestaJugador()
+        {
+            Console.WriteLine("Seleccione el numero junto a la palabra");
+            Console.WriteLine("3-  FIZZ");
+            Console.WriteLine("5-  BUZZ");
+            Console.WriteLine("35- FIZZ BUZZ");
+            Console.WriteLine("0-  Pasar");
+        }
+
+        public void mostrarNumero(int numero)
+        {
+            Console.Clear();
+            Console.WriteLine("\n");
+            Console.WriteLine(numero);
+            Console.WriteLine("\n");
+        }
+
+        public void perdio()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("Has Perdido NAAB");
+            Console.WriteLine("Presione Enter para reiniciar");
+        }
+
     }
 }
