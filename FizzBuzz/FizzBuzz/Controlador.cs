b2f3de796b8db76e468944dc053b808ac61e15d7
Kevin <pkevin.walsh@gmail.com>
2017-07-13 23:16:28 -0600

    Creación de la clase Controlador

---
+++ FizzBuzz/FizzBuzz/Controlador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+
+    class Controlador
+    {
+        Vista vista;
+        Modelo modelo;
+
+        public Controlador(Modelo modelo, Vista vista)
+        {
+            this.modelo = modelo;
+            this.vista = vista;
+        }
+
+        public void iniciarJuego()
+        {
+            this.vista.menuInicial();
+            Console.ReadKey();
+        }
+    }
+}
