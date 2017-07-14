0f836f468b950501eac8eb120637eaf9f403dab9
Kevin <pkevin.walsh@gmail.com>
2017-07-13 23:17:10 -0600

    Creacion de la clase Vista con el menu incial del juego

---
+++ FizzBuzz/FizzBuzz/Vista.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Vista
+    {
+        Modelo modelo;
+
+        public Vista(Modelo modelo)
+        {
+            this.modelo = modelo;
+        }
+
+        public void menuInicial()
+        {
+            Console.WriteLine("Bienvenido al juego FIZZ BUZZ \n");
+            Console.WriteLine("Presione Enter para iniciar");
+        }
+    }
+}
